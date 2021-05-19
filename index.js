906005e8bfa64adb6be7a253be6729c95d20894d
gabrielvicto0802 <gabrirlvictor0802@gmail.com>
2021-05-19 05:05:44 -0300

    subindo loop

---
+++ index.js
@@ -0,0 +1,18 @@
+for(let y= 1;y <=10; y++){
+    console.log(`repetindo for `+ y)
+}
+
+let i= 1
+while(i <= 10) {
+    console.log(`repetindo while `+ i)
+      i++
+}
+
+for (let g=1;g <=100; ++g) {
+     console.log(`numeros impar `+ g++)
+}
+
+for (let g= 0;g <=100; ++g) {
+  console.log(`numeros pares `+ g++)
+ }
+   
